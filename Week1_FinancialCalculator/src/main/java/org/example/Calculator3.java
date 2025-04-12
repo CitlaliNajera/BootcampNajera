e75527512e71d2a9e93bab6e07c39e2e8944b9eb
CitlaliNajera <cnajera@appdev.yearup.org>
2025-04-11 21:01:32 -0500

    Finished my third calculator

--- Week1_FinancialCalculator/src/main/java/org/example/Calculator3.java
+++ Week1_FinancialCalculator/src/main/java/org/example/Calculator3.java
@@ -1,4 +1,39 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Calculator3 {
+    public static void main(String[] args) {
+
+        //user input: monthly payout, expected interest rate, number of years
+        //calculate Present value
+
+        System.out.println("What is the monthly payout for your annuity?");
+
+        Scanner scanner = new Scanner(System.in);
+
+        double PMT = scanner.nextDouble();
+
+        System.out.println("What is the expected interest rate? ");
+
+        double annInt = scanner.nextDouble();
+
+        System.out.println("How many years will it be?");
+
+        double t = scanner.nextDouble();
+
+        double r = (annInt/12) / 100 ;
+
+        double n = t * 12;
+
+        double innerPiece = 1 - Math.pow(1+r,-n);
+
+        double PV = PMT * (innerPiece /r) ;
+
+        System.out.printf("You would need to invest $%.2f today",PV );
+
+
+
+
+    }
 }
