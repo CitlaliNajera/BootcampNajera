fb6547f8a89d9bdf19663c24da263040734f7d7a
CitlaliNajera <cnajera@appdev.yearup.org>
2025-05-15 20:03:09 -0500

    Finished making the contract and sales contract class with methods

--- Workshop4/src/main/java/org/example/Contract.java
+++ Workshop4/src/main/java/org/example/Contract.java
@@ -1,7 +1,71 @@
 package org.example;
 
 public abstract class Contract {
+    private String date;
+    private String customerName;
+    private String customerEmail;
+    private Vehicle vehicle;
+    private double totalPrice;
+    private double monthlyPayment;
 
+    public Contract(String date, String customerName, String customerEmail, Vehicle vehicle) {
+        this.date = date;
+        this.customerName = customerName;
+        this.customerEmail = customerEmail;
+        this.vehicle = vehicle;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerEmail() {
+        return customerEmail;
+    }
+
+    public void setCustomerEmail(String customerEmail) {
+        this.customerEmail = customerEmail;
+    }
+
+    public Vehicle getVehicle() {
+        return vehicle;
+    }
+
+    public void setVehicle(Vehicle vehicle) {
+        this.vehicle = vehicle;
+    }
+
+    public void setTotalPrice(double totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public void setMonthlyPayment(double monthlyPayment) {
+        this.monthlyPayment = monthlyPayment;
+    }
+
+
+    public double gettotalPrice(){
+        return totalPrice;
+    }
+
+
+
+    public double getmonthlyPayment(){
+        return monthlyPayment;
+
+    }
 
 
 }
--- Workshop4/src/main/java/org/example/SalesContract.java
+++ Workshop4/src/main/java/org/example/SalesContract.java
@@ -1,4 +1,61 @@
 package org.example;
 
-public class SalesContract {
-}
+public class SalesContract extends Contract {
+    private boolean financeLoan;
+
+    public SalesContract(String date, String customerName, String customerEmail, Vehicle vehicle, boolean financeLoan) {
+        super(date, customerName, customerEmail, vehicle);
+        this.financeLoan = financeLoan;
+    }
+
+    public boolean isFinanceLoan() {
+        return financeLoan;
+    }
+
+    public void setFinanceLoan(boolean financeLoan) {
+        this.financeLoan = financeLoan;
+    }
+
+    @Override
+    public double gettotalPrice(){
+        int processingFee = 0;
+        final double salesTax= 0.05 * getVehicle().getPrice();
+        final double vehicleAmount = getVehicle().getPrice();
+
+        if (getVehicle().getPrice()<10000){
+            processingFee=295;
+
+        }
+        else if(getVehicle().getPrice()>=10000){
+            processingFee=495;
+        }
+
+        return 100 + processingFee + salesTax + vehicleAmount;
+    }
+    @Override
+    public double getmonthlyPayment(){
+        if (!financeLoan) {
+            return 0.0;
+        }
+        double interestRate;
+        int loanTerm;
+
+        if (gettotalPrice() < 10000) {
+            interestRate = 5.25 / 100;
+            loanTerm = 24;
+        } else {
+            interestRate = 4.25 / 100;
+            loanTerm = 48;
+        }
+
+        double monthlyRate = interestRate / 12;
+
+        return (gettotalPrice() * monthlyRate) / (1 - Math.pow(1 + monthlyRate, -loanTerm));
+    }
+
+
+
+
+        }
+
+
